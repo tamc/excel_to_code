25467b987dcdae0baf0b606e62191ad09eb9d93a
Tom Counsell <tom@counsell.org>
2011-12-29 14:54:25 +0000

    Renamed spec/excel_fragments to spec/test_data

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'rspec'
 
 def excel_fragment(name)
-  File.open(File.join(File.dirname(__FILE__),'excel_fragments',name))
+  File.open(File.join(File.dirname(__FILE__),'test_data',name))
 end
+
+alias :test_data :excel_fragment
