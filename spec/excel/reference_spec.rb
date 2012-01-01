f72d628dbf3145f8775898f10ac8047c4f7a0b11
Tom Counsell <tom@counsell.org>
2012-01-01 11:51:29 +0000

    Refactored Reference to be a subclass of string and to return only one instance per reference

--- spec/excel/reference_spec.rb
+++ spec/excel/reference_spec.rb
@@ -2,14 +2,22 @@ require_relative '../spec_helper'
 require_relative '../../src/excel/reference'
 
 describe Reference do
-  it "should take excel references as its initializer and return excel references when called with to_s" do
-    Reference.new("A1").to_s.should == "A1"
-    Reference.new("AAA$305").to_s.should == "AAA$305"
+  it "should take have a Reference#for method that takes a reference string" do
+    Reference.for("A1").should == "A1"
+    Reference.for("AAA$305").should == "AAA$305"
+  end
+  
+  it "should be a subclass of String" do
+    Reference.for("A1").should be_kind_of(String)
+  end
+  
+  it "should return the same instance each time a particular reference is required" do
+    Reference.for("A1").object_id.should ==  Reference.for("A1").object_id
   end
   
   it "should be able to offset a reference" do
-    Reference.new("A1").offset(0,0).to_s.should == "A1"
-    Reference.new("A1").offset(1,1).to_s.should == "B2"
-    Reference.new("Z1").offset(1,1).to_s.should == "AA2"
+    Reference.for("A1").offset(0,0).should == "A1"
+    Reference.for("A1").offset(1,1).should == "B2"
+    Reference.for("Z1").offset(1,1).should == "AA2"
   end
 end
--- src/excel/reference.rb
+++ src/excel/reference.rb
@@ -1,5 +1,17 @@
-class Reference
+class Reference < String
   
+  # This is so that we only have one instance for a given reference
+  @@references_for_text ||= Hash.new do |hash,text|
+    hash[text] = reference = Reference.new(text)
+    reference
+  end
+    
+  # This is so that we only have one instance of a given reference specified by its variables
+  def Reference.for(text)
+     @@references_for_text[text]
+  end
+    
+  # This caches the calculation for turning column letters (e.g., AAB) into column numbers (e.g., 127)
   @@column_number_for_column ||= Hash.new do |hash,letters|
     number = letters.downcase.each_byte.to_a.reverse.each.with_index.inject(0) do |memo,byte_with_index,c|
       memo + ((byte_with_index.first - 96) * (26**byte_with_index.last))
@@ -9,6 +21,7 @@ class Reference
     number
   end
   
+  # This caches the calculation for turning column numbers (e.g., 127) into column letters (e.g., AAB)
   @@column_letters_for_column_number ||= Hash.new do |hash,number|
     letters = (number-1).to_i.to_s(26)
     letters = (letters[0...-1].tr('1-9a-z','abcdefghijklmnopqrstuvwxyz') + letters[-1,1].tr('0-9a-z','abcdefghijklmnopqrstuvwxyz')).gsub('a0','z').gsub(/([b-z])0/) { $1.tr('b-z','a-y')+"z" }
@@ -18,23 +31,18 @@ class Reference
     letters
   end
   
-  def initialize(text)
-    text =~ /(\$)?([A-Za-z]{1,3})(\$)?([0-9]+)/
-    @fc, @c, @fr, @r = $1, $2, $3, $4
+  def calculate_excel_variables
+    return if @excel_variables_calculated
+    self =~ /(\$)?([A-Za-z]{1,3})(\$)?([0-9]+)/
+    @excel_fixed_column, @excel_column, @excel_fixed_row, @excel_row = $1, $2, $3, $4
+    @excel_variables_calculated = true
   end
     
   def offset(rows,columns)
-    dup.offset!(rows,columns)
+    calculate_excel_variables
+    new_column = @@column_letters_for_column_number[@@column_number_for_column[@excel_column] + columns]
+    new_row = @excel_row.to_i + rows
+    Reference.new([@excel_fixed_column,new_column,@excel_fixed_row,new_row].join)
   end
   
-  def offset!(rows,columns)
-    @r = @r.to_i + rows
-    @c = @@column_letters_for_column_number[@@column_number_for_column[@c] + columns]
-    self
-  end
-  
-  def to_s
-    "#{@fc && '$'}#{@c}#{@fr && '$'}#{@r}"
-  end
-
 end
