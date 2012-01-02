f618a35b9c074fcfdeccbf994f7f9c8dfe196898
Tom Counsell <tom@counsell.org>
2012-01-02 20:19:26 +0000

    excel_to_ruby now extracts the basic information relating to each worksheet

--- src/commands/excel_to_ruby.rb
+++ src/commands/excel_to_ruby.rb
@@ -10,6 +10,7 @@ class ExcelToRuby
     sort_out_output_directories
     unzip_excel
     process_workbook
+    initial_extract_from_worksheets
   end
   
   def sort_out_output_directories
@@ -25,19 +26,65 @@ class ExcelToRuby
 
   # Extract the shared strings    
   # Extract the sheet names, initialy with relationship references
-  # Extract the workbook relationships  
+  # Extract the workbook relationships
+  # Use these to create the sheet names and filenames
   def process_workbook    
     extract ExtractSharedStrings, 'sharedStrings.xml', 'shared_strings'
     extract ExtractWorksheetNames, 'workbook.xml', 'worksheet_names_without_filenames'
-    extract ExtractRelationships, '_rels/workbook.xml.rels', 'workbook_relationships'
+    extract ExtractRelationships, File.join('_rels','workbook.xml.rels'), 'workbook_relationships'
     rewrite RewriteWorksheetNames, 'worksheet_names_without_filenames', 'workbook_relationships', 'worksheet_names'
   end
   
+  def initial_extract_from_worksheets
+    worksheets do |name,xml_filename|
+      initial_extract_from_worksheet(name,xml_filename)
+    end
+  end
+  
+  
+  # Extracts:
+  # Values
+  # Formulae (simple, shared and array)
+  # Rewrites the formulae to ast
+  # Rewrites the values to replace shared strings with their values
+  def initial_extract_from_worksheet(name,xml_filename)
+    worksheet_directory = File.join(output_directory,'intermediate',name)
+    FileUtils.mkdir(worksheet_directory)
+    worksheet_xml = File.open(xml_filename,'r')
+    {
+      ExtractValues => 'values', 
+      ExtractSimpleFormulae => 'simple_formulae',
+      ExtractSharedFormulae => 'shared_formulae',
+      ExtractArrayFormulae => 'array_formulae'
+    }.each do |_klass,output_filename|
+      worksheet_xml.rewind
+      extract _klass, worksheet_xml, File.join(name,output_filename)
+      if _klass == ExtractValues
+        rewrite RewriteValuesToIncludeSharedStrings, File.join(name,output_filename), 'shared_strings', File.join(name,"#{output_filename}_no_shared_strings")
+      else
+        rewrite RewriteFormulaeToAst, File.join(name,output_filename), File.join(name,"#{output_filename}.ast")
+      end
+        
+    end
+    close(worksheet_xml)
+  end
+  
+  def worksheets
+    IO.readlines(File.join(output_directory,'intermediate','worksheet_names')).each do |line|
+      name, filename = *line.split("\t")
+      filename = File.expand_path(File.join(xml_dir,'xl',filename.strip))
+      yield name, filename
+    end
+  end
+  
   def extract(_klass,xml_name,output_name)
-    i = xml(xml_name)
+    i = xml_name.is_a?(String) ? xml(xml_name) : xml_name
     o = output(output_name)
     _klass.extract(i,o)
-    close(i,o)
+    if xml_name.is_a?(String)
+      close(i)
+    end
+    close(o)
   end
   
   def rewrite(_klass,*args)
