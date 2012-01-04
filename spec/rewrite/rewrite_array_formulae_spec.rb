3a7f2c715783c77ef404a10fef61d79cee35aa64
Tom Counsell <tom@counsell.org>
2012-01-04 21:24:21 +0000

    Fixed array formula rewrite to cope with single cell array formulae

--- spec/rewrite/rewrite_array_formulae_spec.rb
+++ spec/rewrite/rewrite_array_formulae_spec.rb
@@ -2,7 +2,7 @@ require_relative '../spec_helper'
 
 describe RewriteArrayFormulae do
   
-  it "should take array formula like 'reference\\array range\\tformula ast\\n' and output a series of 'reference\\tformula ast\\n' for the array formulae" do
+  it "should take array formula like 'reference\\array range\\tformula ast\\n' and output ARRAYFORMULA() and CONTINUE() functions, similar to Google spreadsheet appraoch" do
     input = StringIO.new(%Q|B6\tB6:B8\t[:formula, [:arithmetic, [:area, "B3", "B5"], [:operator, "+"], [:area, "C3", "C5"]]]\n|)
     output = StringIO.new
     RewriteArrayFormulae.rewrite(input,output)
@@ -13,4 +13,16 @@ B8\t[:formula, [:function, "CONTINUE", [:cell, "B6"], 3, 1]]
 END
     output.string.should == expected_output
   end
+
+  it "should work with single cell array formulae" do
+    input = StringIO.new(%Q|B6\tB6\t[:formula, [:arithmetic, [:area, "B3", "B5"], [:operator, "+"], [:area, "C3", "C5"]]]\n|)
+    output = StringIO.new
+    RewriteArrayFormulae.rewrite(input,output)
+    expected_output = <<END
+B6\t[:formula, [:function, "ARRAYFORMULA", [:arithmetic, [:area, "B3", "B5"], [:operator, "+"], [:area, "C3", "C5"]]]]
+END
+    output.string.should == expected_output
+  end
+
+
 end
