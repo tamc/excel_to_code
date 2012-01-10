6dd531a2baefaf6fb95cd60d875cb17cbf30e22c
Tom Counsell <tom@counsell.org>
2012-01-10 08:46:36 +0000

    Refactored the way that map_formulae_to_ruby.rb is tested

---
+++ spec/compile/ruby/map_formulae_to_ruby_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../../spec_helper'
+
+describe MapFormulaeToRuby do
+    
+  before(:all) do 
+    @mapper = MapFormulaeToRuby.new
+  end
+  
+  checks = test_data('formulae_to_ruby.txt').lines.map.with_index { |line,i| [i,line] }.find_all { |line| line[1].start_with?('[:') }.map { |line| [line[0],*line[1].split("\t")] }
+  checks.each do |c|
+    it "converts #{c[1].strip} into #{c[2].strip} (line #{c[0]+1} of formulae_to_ast.txt)" do
+      desired = c[2].strip
+      actual = @mapper.map(eval(c[1]))
+      actual.to_s.should == desired.to_s
+    end
+  end
+end
