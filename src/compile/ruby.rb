4a4e1b644bef9137d42691619cc6a6eaa89ab97b
Tom Counsell <tom@counsell.org>
2012-01-13 20:50:08 +0000

    Added a map_sheet_names_to_ruby_names function

--- src/compile/ruby.rb
+++ src/compile/ruby.rb
@@ -1,2 +1,3 @@
 require_relative 'ruby/compile_to_ruby'
 require_relative 'ruby/compile_to_ruby_unit_test'
+require_relative 'ruby/map_sheet_names_to_ruby_names'
---
+++ src/compile/ruby/map_sheet_names_to_ruby_names.rb
@@ -0,0 +1,19 @@
+class MapSheetNamesToRubyNames
+  
+  def self.rewrite(*args)
+    self.new.rewrite(*args)
+  end
+  
+  def rewrite(input,output)
+    ruby_names_assigned = {}
+    input.lines do |line|
+      excel_worksheet_name = line.split("\t").first
+      ruby_name = excel_worksheet_name.downcase.gsub(/[^a-z0-9]+/,'_')
+      ruby_name = "_"+ruby_name if ruby_name[0] =~ /[0-9]/
+      ruby_name = ruby_name + "2" if ruby_names_assigned.has_key?(ruby_name)
+      ruby_name.succ! while ruby_names_assigned.has_key?(ruby_name)
+      output.puts "#{excel_worksheet_name}\t#{ruby_name}"
+      ruby_names_assigned[ruby_name] = excel_worksheet_name
+    end
+  end
+end
