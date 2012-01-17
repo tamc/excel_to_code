1f1e8b80924f1010a2f8039907207b9ccb836cc7
Tom Counsell <tom@counsell.org>
2012-01-17 08:32:30 +0000

    Added replace_indirects_with_references

---
+++ spec/simplify/replace_indirects_with_references_spec.rb
@@ -0,0 +1,26 @@
+require_relative '../spec_helper'
+
+describe ReplaceIndirectsWithReferences do
+  
+it "should replace INDIRECT() functions with the reference that they refer to, if they have been passed a string value" do
+
+input = <<END
+A1\t[:function,"INDIRECT", [:string,"$A$5"]]
+A2\t[:function,"INDIRECT", [:cell,"$A$5"]]
+END
+
+expected_output = <<END
+A1\t[:cell, "$A$5"]
+A2\t[:function, "INDIRECT", [:cell, "$A$5"]]
+END
+    
+input = StringIO.new(input)
+output = StringIO.new
+r = ReplaceIndirectsWithReferences.new
+r.replace(input,output)
+output.string.should == expected_output
+
+end # / it
+
+
+end # / describe
---
+++ src/simplify/replace_indirects_with_references.rb
@@ -0,0 +1,43 @@
+require_relative '../excel/formula_peg'
+
+class ReplaceIndirectsWithReferencesAst
+    
+  def map(ast)
+    return ast unless ast.is_a?(Array)
+    operator = ast[0]
+    if respond_to?(operator)
+      send(operator,*ast[1..-1])
+    else
+      [operator,*ast[1..-1].map {|a| map(a) }]
+    end
+  end
+  
+  def function(name,*args)
+    if name == "INDIRECT" && args.size == 1 && args[0][0] == :string
+      Formula.parse(args[0][1]).to_ast[1]
+    else
+      [:function,name,*args]
+    end
+  end
+end
+  
+
+class ReplaceIndirectsWithReferences
+    
+  def self.replace(*args)
+    self.new.replace(*args)
+  end
+  
+  def replace(input,output)
+    rewriter = ReplaceIndirectsWithReferencesAst.new
+    input.lines do |line|
+      # Looks to match lines with references
+      if line =~ /"INDIRECT"/
+        ref, ast = line.split("\t")
+        output.puts "#{ref}\t#{rewriter.map(eval(ast)).inspect}"
+      else
+        output.puts line
+      end
+    end
+  end
+end
