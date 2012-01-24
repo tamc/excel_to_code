1ee04e88d2b747dea24e7324b481d9382c6f3144
Tom Counsell <tom@counsell.org>
2012-01-24 08:47:45 +0000

    Debug messages

--- src/extract/extract_worksheet_dimensions.rb
+++ src/extract/extract_worksheet_dimensions.rb
@@ -2,6 +2,7 @@ require_relative 'simple_extract_from_xml'
 
 class ExtractWorksheetDimensions < SimpleExtractFromXML
   
+  # FIXME: Is there an elegant way to abort once we have found the dimension tag?
   def start_element(name,attributes)
     return false unless name == "dimension"
     output.puts "#{attributes.assoc('ref').last}"
