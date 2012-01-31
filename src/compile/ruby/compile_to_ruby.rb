f9a02efafe19ccea03937be75e3e44649445c513
Tom Counsell <tom@counsell.org>
2012-01-31 20:53:04 +0000

    Now reports the contents of line if compiling to ruby hits an error

--- src/compile/ruby/compile_to_ruby.rb
+++ src/compile/ruby/compile_to_ruby.rb
@@ -13,13 +13,18 @@ class CompileToRuby
     mapper = MapFormulaeToRuby.new
     mapper.sheet_names = Hash[sheet_names_file.readlines.map { |line| line.strip.split("\t")}]
     input.lines do |line|
-      ref, formula = line.split("\t")
-      if settable.call(ref)
-        output.puts "  attr_accessor :#{ref.downcase} # Default: #{mapper.map(eval(formula))}"
-        defaults.puts "    @#{ref.downcase} = #{mapper.map(eval(formula))}" if defaults
-      else
-        output.puts "  def #{ref.downcase}; #{mapper.map(eval(formula))}; end"
-      end
+      begin
+        ref, formula = line.split("\t")
+        if settable.call(ref)
+          output.puts "  attr_accessor :#{ref.downcase} # Default: #{mapper.map(eval(formula))}"
+          defaults.puts "    @#{ref.downcase} = #{mapper.map(eval(formula))}" if defaults
+        else
+          output.puts "  def #{ref.downcase}; #{mapper.map(eval(formula))}; end"
+        end
+      rescue Exception => e
+        puts "Exception at line #{line}"
+        raise
+      end      
     end
   end
   
