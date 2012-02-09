f19ad0cdcd934ddd124f123dce7e561d9b31eeef
Tom Counsell <tom@counsell.org>
2012-02-09 20:36:55 +0000

    Changed tests for numeric values to use assert_in_epsilon rather than asser_equal

--- spec/commands/excel_to_ruby_output_expected/ruby/tests/test_referencing.rb
+++ spec/commands/excel_to_ruby_output_expected/ruby/tests/test_referencing.rb
@@ -5,8 +5,8 @@ require_relative '../examplespreadsheet'
 module ExampleSpreadsheet
 class TestReferencing < Test::Unit::TestCase
   def worksheet; Referencing.new; end
-  def test_a4; assert_equal(10,worksheet.a4); end
-  def test_b4; assert_equal(11,worksheet.b4); end
-  def test_c4; assert_equal(12,worksheet.c4); end
+  def test_a4; assert_in_epsilon(10,worksheet.a4); end
+  def test_b4; assert_in_epsilon(11,worksheet.b4); end
+  def test_c4; assert_in_epsilon(12,worksheet.c4); end
 end
 end
--- spec/commands/excel_to_ruby_output_expected/ruby/tests/test_tables.rb
+++ spec/commands/excel_to_ruby_output_expected/ruby/tests/test_tables.rb
@@ -5,6 +5,6 @@ require_relative '../examplespreadsheet'
 module ExampleSpreadsheet
 class TestTables < Test::Unit::TestCase
   def worksheet; Tables.new; end
-  def test_a1; assert_equal(12,worksheet.a1); end
+  def test_a1; assert_in_epsilon(12,worksheet.a1); end
 end
 end
