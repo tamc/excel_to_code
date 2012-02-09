e7c3cc58cfc111bc80874d385f7b2b85563c374c
Tom Counsell <tom@counsell.org>
2012-02-09 20:36:31 +0000

    Changed tests for numeric values to use assert_in_epsilon rather than asser_equal

--- spec/compile/ruby/compile_to_ruby_unit_test_spec.rb
+++ spec/compile/ruby/compile_to_ruby_unit_test_spec.rb
@@ -22,12 +22,12 @@ A6\t[:number, "0"]
 END
 
 expected = <<END
-  def test_a1; assert_equal(1,worksheet.a1); end
+  def test_a1; assert_in_epsilon(1,worksheet.a1); end
   def test_a2; assert_equal("Hello",worksheet.a2); end
   def test_a3; assert_equal(:name,worksheet.a3); end
   def test_a4; assert_equal(true,worksheet.a4); end
   def test_a5; assert_equal(false,worksheet.a5); end
-  def test_a6; assert_equal(0,worksheet.a6 || 0); end
+  def test_a6; assert_in_epsilon(0,worksheet.a6 || 0); end
 END
 
 compile(input).should == expected
--- src/compile/ruby/compile_to_ruby_unit_test.rb
+++ src/compile/ruby/compile_to_ruby_unit_test.rb
@@ -10,10 +10,15 @@ class CompileToRubyUnitTest
     mapper = MapValuesToRuby.new
     input.lines do |line|
       ref, formula = line.split("\t")
-      value = mapper.map(eval(formula))
+      ast = eval(formula)
+      value = mapper.map(ast)
       full_reference = "worksheet.#{ref.downcase}"
-      if value == "0" # Need to do a slightly different test, because needs to pass if nil returned, as well as zero
-        output.puts "  def test_#{ref.downcase}; assert_equal(#{value},#{full_reference} || 0); end"      
+      if ast.first == :number
+        if value == "0" # Need to do a slightly different test, because needs to pass if nil returned, as well as zero
+          output.puts "  def test_#{ref.downcase}; assert_in_epsilon(#{value},#{full_reference} || 0); end"      
+        else
+          output.puts "  def test_#{ref.downcase}; assert_in_epsilon(#{value},#{full_reference}); end"      
+        end
       else
         output.puts "  def test_#{ref.downcase}; assert_equal(#{value},#{full_reference}); end"
       end
