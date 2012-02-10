f458dc54005f263d48ce61793887cdead99ad9b5
Tom Counsell <tom@counsell.org>
2012-02-10 17:21:47 +0000

    Add more tests for the table reference replacement code

--- spec/simplify/replace_table_references_spec.rb
+++ spec/simplify/replace_table_references_spec.rb
@@ -58,5 +58,29 @@ r.replace(input,tables,output)
 output.string.should == expected_output
 end # /it
 
+it "should work when applied to array references" do
+
+input = <<END
+F15\t[:table_reference, "I.a.Inputs", "#Headers"]
+F15\t[:function, "INDEX", [:function, "INDIRECT", [:string_join, [:string, "'"], [:table_reference, "I.a.Inputs", "#Headers"], [:string, "'!Year.Matrix"]]], [:function, "MATCH", [:string_join, [:string, "Subtotal."], [:cell, "$A$2"]], [:function, "INDIRECT", [:string_join, [:string, "'"], [:table_reference, "I.a.Inputs", "#Headers"], [:string, "'!Year.Modules"]]], [:number, "0"]], [:function, "MATCH", [:table_reference, "I.a.Inputs", "Vector"], [:function, "INDIRECT", [:string_join, [:string, "'"], [:table_reference, "I.a.Inputs", "#Headers"], [:string, "'!Year.Vectors"]]], [:number, "0"]]]
+END
+
+tables = <<END
+I.a.Inputs	I.a	C14:O16	0	Vector	Name	Notes	2007	2010	2015	2020	2025	2030	2035	2040	2045	2050
+END
+
+expected_output = <<END
+F15\t[:sheet_reference, "I.a", [:cell, "F14"]]
+F15\t[:function, "INDEX", [:function, "INDIRECT", [:string_join, [:string, "'"], [:sheet_reference, "I.a", [:cell, "F14"]], [:string, "'!Year.Matrix"]]], [:function, "MATCH", [:string_join, [:string, "Subtotal."], [:cell, "$A$2"]], [:function, "INDIRECT", [:string_join, [:string, "'"], [:sheet_reference, "I.a", [:cell, "F14"]], [:string, "'!Year.Modules"]]], [:number, "0"]], [:function, "MATCH", [:sheet_reference, "I.a", [:cell, "C15"]], [:function, "INDIRECT", [:string_join, [:string, "'"], [:sheet_reference, "I.a", [:cell, "F14"]], [:string, "'!Year.Vectors"]]], [:number, "0"]]]
+END
+    
+input = StringIO.new(input)
+tables = StringIO.new(tables)
+output = StringIO.new
+r = ReplaceTableReferences.new
+r.sheet_name = "I.a"
+r.replace(input,tables,output)
+output.string.should == expected_output
+end # /it
 
 end # /describe
