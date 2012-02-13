91e1bb84e1367478b07aae003c743260d1e7ba65
Tom Counsell <tom@counsell.org>
2012-02-13 07:47:56 +0000

    rewrite of shared formulae now copes with shared formulae that are not top left in their sharing range

--- spec/rewrite/rewrite_shared_formulae_spec.rb
+++ spec/rewrite/rewrite_shared_formulae_spec.rb
@@ -2,17 +2,31 @@ require_relative '../spec_helper'
 
 describe RewriteSharedFormulae do
   
-  it "should take shared formula like 'reference\\tsharing range\\tformula ast\\n' and output a series of 'reference\\tformula ast\\n' for the shared formulae" do
-    input = StringIO.new("B3\tB3:C4\t[:cell,'A1']\nZ5\tZ5\t[:cell,'A1']\n")
-    output = StringIO.new
-    RewriteSharedFormulae.rewrite(input,output)
-    expected_output = <<END
+it "should take shared formula like 'reference\\tsharing range\\tformula ast\\n' and output a series of 'reference\\tformula ast\\n' for the shared formulae" do
+input = StringIO.new("B3\tB3:C4\t[:cell,'A1']\nZ5\tZ5\t[:cell,'A1']\n")
+output = StringIO.new
+RewriteSharedFormulae.rewrite(input,output)
+expected_output = <<END
 B3	[:cell, "A1"]
 B4	[:cell, "A2"]
 C3	[:cell, "B1"]
 C4	[:cell, "B2"]
 Z5	[:cell, "A1"]
 END
-    output.string.should == expected_output
-  end
+output.string.should == expected_output
+end
+
+it "should take cope with occasions when the shared formula is not the top left in the range" do
+input = StringIO.new("C3\tB3:C4\t[:cell,'B1']\n")
+output = StringIO.new
+RewriteSharedFormulae.rewrite(input,output)
+expected_output = <<END
+B3	[:cell, "A1"]
+B4	[:cell, "A2"]
+C3	[:cell, "B1"]
+C4	[:cell, "B2"]
+END
+output.string.should == expected_output
+end
+
 end
--- src/rewrite/rewrite_shared_formulae.rb
+++ src/rewrite/rewrite_shared_formulae.rb
@@ -8,23 +8,30 @@ class RewriteSharedFormulae
   def rewrite(input,output)
     input.lines do |line|
       ref, copy_range, formula = line.split("\t")
-      share_formula(formula,copy_range,output)
+      share_formula(ref,formula,copy_range,output)
     end
   end
   
-  def share_formula(formula,copy_range,output)
+  def share_formula(ref,formula,copy_range,output)
     shared_ast = eval(formula)
     copier = AstCopyFormula.new
     copy_range = Area.for(copy_range)
     copy_range.calculate_excel_variables
+    
     start_reference = copy_range.excel_start
     
+    r = Reference.for(ref)
+    r.calculate_excel_variables
+    
+    offset_from_formula_to_start_rows = start_reference.excel_row_number - r.excel_row_number
+    offset_from_formula_to_start_columns = start_reference.excel_column_number - r.excel_column_number
+    
     copy_range.offsets.each do |row,column|
-      ref = start_reference.offset(row,column)
-      copier.rows_to_move = row
-      copier.columns_to_move = column
+      new_ref = start_reference.offset(row,column)
+      copier.rows_to_move = row + offset_from_formula_to_start_rows
+      copier.columns_to_move = column + offset_from_formula_to_start_columns
       ast = copier.copy(shared_ast)
-      output.puts "#{ref}\t#{ast.inspect}"
+      output.puts "#{new_ref}\t#{ast.inspect}"
     end
   end
   
