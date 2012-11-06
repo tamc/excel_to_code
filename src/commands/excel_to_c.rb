74bb09ca9c1618333e8730783e993653d86a4e12
Tom Counsell <tom@counsell.org>
2012-11-06 08:56:45 +0000

    Adding shim for getting and setting ranges

--- src/commands/excel_to_c.rb
+++ src/commands/excel_to_c.rb
@@ -192,6 +192,18 @@ class #{ruby_module_name}Shim
     when :ExcelString; excel_value[:string].read_string.force_encoding("utf-8")
     when :ExcelBoolean; excel_value[:number] == 1
     when :ExcelEmpty; nil
+    when :ExcelRange
+      r = excel_value[:rows]
+      c = excel_value[:columns]
+      p = excel_value[:array]
+      s = #{ruby_module_name}::ExcelValue.size
+      a = Array.new(r) { Array.new(c) }
+      (0...r).each do |row|
+        (0...c).each do |column|
+          a[row][column] = ruby_from_excel_value(#{ruby_module_name}::ExcelValue.new(p + (((row*c)+column)*s)))
+        end
+      end 
+      return a
     when :ExcelError; [:value,:name,:div0,:ref,:na][excel_value[:number]]
     else
       raise Exception.new("ExcelValue type \u0023{excel_value[:type].inspect} not recognised")
@@ -202,7 +214,10 @@ class #{ruby_module_name}Shim
     name = name.to_s
     name = "set_\#{name[0..-2]}" if name.end_with?('=')
     return false unless #{ruby_module_name}.respond_to?(name)
-    excel_value = #{ruby_module_name}::ExcelValue.new
+    Getsetranges.send(name, excel_value_from_ruby_value(ruby_value))
+  end
+
+  def excel_value_from_ruby_value(ruby_value, excel_value = #{ruby_module_name}::ExcelValue.new)
     case ruby_value
     when Numeric
       excel_value[:type] = :ExcelNumber
@@ -210,18 +225,34 @@ class #{ruby_module_name}Shim
     when String
       excel_value[:type] = :ExcelString
       excel_value[:string] = FFI::MemoryPointer.from_string(ruby_value.encode('utf-8'))
-    when Boolean
+    when TrueClass, FalseClass
       excel_value[:type] = :ExcelBoolean
       excel_value[:number] = ruby_value ? 1 : 0
     when nil
       excel_value[:type] = :ExcelEmpty
+    when Array
+      excel_value[:type] = :ExcelRange
+      # Presumed to be a row unless specified otherwise
+      if ruby_value.first.is_a?(Array)
+        excel_value[:rows] = ruby_value.size
+        excel_value[:columns] = ruby_value.first.size
+      else
+        excel_value[:rows] = 1
+        excel_value[:columns] = ruby_value.size
+      end
+      ruby_values = ruby_value.flatten
+      pointer = FFI::MemoryPointer.new(#{ruby_module_name}::ExcelValue, ruby_values.size)
+      excel_value[:array] = pointer
+      ruby_values.each.with_index do |v,i|
+        excel_value_from_ruby_value(v, #{ruby_module_name}::ExcelValue.new(pointer[i]))
+      end
     when Symbol
       excel_value[:type] = :ExcelError
       excel_value[:number] = [:value, :name, :div0, :ref, :na].index(ruby_value)
     else
       raise Exception.new("Ruby value \u0023{ruby_value.inspect} not translatable into excel")
     end
-    #{ruby_module_name}.send(name, excel_value)
+    excel_value
   end
 
 end
