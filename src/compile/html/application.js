9d0fa70b1acd66e0732c1a37f74449dc164ffe12
Tom Counsell <tom@counsell.org>
2013-01-08 19:15:12 +0000

    Attempt to make the selected cell in generated html scroll to the middle of the screen (doesn't quite work)

--- src/compile/html/application.js
+++ src/compile/html/application.js
@@ -7,6 +7,15 @@ $(document).ready(function() {
     $(window).resize(setSizes);
     setSizes();
 
+    var scrollTo = function(element) {
+      var element = $(element);
+      var worksheet = $('#worksheet');
+      worksheet.animate({ 
+          scrollTop: element.offset().top + (element.height()/2) - (worksheet.height()/2), 
+          scrollLeft: element.offset().left + (element.width()/2) - (worksheet.width()/2)
+      }, 500);
+    };
+
     $('table.cells td').on('click', function(event) {
       window.location.hash = this.id.substring(1);
     });
@@ -21,6 +30,7 @@ $(document).ready(function() {
       c = $(".c"+reference)
       $('#selectedcell').html(reference);
       $('#selectedformula').html(c.data('formula'));
+      $('#selectedformula a').on('click', function(event) { scrollTo(this) });
     }
 
     $(window).on('hashchange', function(event) {
@@ -35,5 +45,5 @@ $(document).ready(function() {
       $(window).trigger('hashchange'); // Doesn't happen on page load
     };
 
-
+    scrollTo($('.selected'));
 });
