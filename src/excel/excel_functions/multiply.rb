5e570f9b60f28eb9f01643b5af13e86344a6aa59
Tom Counsell <tom@counsell.org>
2013-07-02 06:56:26 +0100

    Remove obsolete error handling code from multiply.rb

--- src/excel/excel_functions/multiply.rb
+++ src/excel/excel_functions/multiply.rb
@@ -4,9 +4,6 @@ require_relative 'apply_to_range'
 module ExcelFunctions
   
   def multiply(a,b)
-  begin
-    # return apply_to_range(a,b) { |a,b| multiply(a,b) } if a.is_a?(Array) || b.is_a?(Array)
-    
     a = number_argument(a)
     b = number_argument(b)
     
@@ -14,11 +11,6 @@ module ExcelFunctions
     return b if b.is_a?(Symbol)
     
     a * b
-    
-  rescue Error => e
-    print e.backtrace.join('\n')
-    raise
-  end
   end
   
 end
