0ae3bd9a70c9493ea9b658c7117865b831bd7862
Tom Counsell <tom@counsell.org>
2013-08-01 18:15:36 +0100

    Added an example that has utf-8 strings and an indirect

---
+++ examples/make-utf8-strings.rb
@@ -0,0 +1,12 @@
+require_relative '../src/excel_to_code'
+this_directory = File.dirname(__FILE__)
+command = ExcelToC.new
+# command = ExcelToRuby.new
+command.excel_file = File.join(this_directory,'utf8-strings.xlsx')
+command.output_directory = this_directory
+command.output_name = 'utf8strings'
+command.actually_compile_code = true
+command.actually_run_tests = true
+command.cells_that_can_be_set_at_runtime = { '2015' => ['B3'] }
+command.run_in_memory = true
+command.go!
