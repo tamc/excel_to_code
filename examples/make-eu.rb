a3f73fed3f29624384dc892caee60c237b92f330
Tom Counsell <tom@counsell.org>
2013-12-04 16:05:45 +0000

    Updating example

--- examples/make-eu.rb
+++ examples/make-eu.rb
@@ -1,7 +1,7 @@
 require_relative '../src/excel_to_code'
 this_directory = File.dirname(__FILE__)
-#command = ExcelToC.new
-command = ExcelToRuby.new
+command = ExcelToC.new
+#command = ExcelToRuby.new
 command.excel_file = File.join(this_directory,'eu.xlsx')
 command.output_directory = this_directory
 command.output_name = 'eu'
