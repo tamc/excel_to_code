ccb06fb38694b75b79a80e3bcd86fbed0a2b4861
Tom Counsell <tom@counsell.org>
2013-12-10 09:21:41 +0000

    Remove debugging statements

--- src/commands/excel_to_x.rb
+++ src/commands/excel_to_x.rb
@@ -757,7 +757,6 @@ class ExcelToX
     begin 
       number_of_passes += 1
       log.info "Starting pass #{number_of_passes} on #{cells_with_formulae.size} cells"
-      p @formulae[[:'Intermediate output', :AY7]]
 
       replacements_made_in_the_last_pass = 0
       inline_replacer.count_replaced = 0
@@ -767,8 +766,6 @@ class ExcelToX
       indirect_replacement.count_replaced = 0
       references_that_need_updating = {}
 
-      p @formulae[[:'Intermediate output', :AY7]]
-    
       cells_with_formulae.each do |ref, ast|
         # FIXME: Shouldn't need to wrap ref.fist in an array
         inline_replacer.current_sheet_name = [ref.first]
@@ -786,7 +783,6 @@ class ExcelToX
         end
         cells_with_formulae.delete(ref) if VALUE_TYPE[ast[0]]
       end
-      p @formulae[[:'Intermediate output', :AY7]]
 
       simplify(references_that_need_updating)
 
