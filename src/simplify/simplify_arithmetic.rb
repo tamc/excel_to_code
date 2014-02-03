4e7a899b4fe391b7ed44aca9b648c34d7f3b23f7
Tom Counsell <tom@counsell.org>
2014-02-03 09:09:23 +0000

    Improved the speed of simplifying arithmetic by removing redundant passes

--- src/simplify/simplify_arithmetic.rb
+++ src/simplify/simplify_arithmetic.rb
@@ -1,48 +1,54 @@
 class SimplifyArithmeticAst
-    
+  
+  attr_accessor :map_count  
+
   def map(ast)
+    @map_count = 0
+    @brackets_to_remove = []
+    simplify_arithmetic(ast)
+    remove_brackets(ast)
+  end
+
+  def simplify_arithmetic(ast)
+    @map_count += 1
     return ast unless ast.is_a?(Array)
+    ast.each { |a| simplify_arithmetic(a) }
     case ast[0]
-    when :brackets; brackets(ast)
     when :arithmetic; arithmetic(ast)
+    when :brackets; @brackets_to_remove << ast
     end
-    ast.each { |a| map(a) }
     ast
   end
   
-  def brackets(ast)
-    raise NotSupportedException.new("Multiple arguments not supported in brackets #{args.inspect}") if ast.size > 2
-    ast.replace(map(ast[1]))
+  def remove_brackets(ast)
+    @brackets_to_remove.uniq.each do |ast|
+      raise NotSupportedException.new("Multiple arguments not supported in brackets #{ast.inspect}") if ast.size > 2
+      ast.replace(ast[1])
+    end
+    ast
   end
   
   def arithmetic(ast)
-    case ast.size
-    when 2; return map(ast[1]) # Not really arithmetic
-    when 4; # Normal arithmetic that doesn't need re-arranging
-      ast.each { |a| map(a) }
-    else
-      # This sets the operator precedence
-      i = nil
-      [{:'^' => 1},{:'*' => 2,:'/' => 2},{:'+' => 3,:'-' => 3}].each do |op|
-        i = ast.find_index { |a| a[0] == :operator && op.has_key?(a[1])}
-        break if i
-      end
-      if i
-        # Now we need to wrap that operation in its own arithmetic clause
-        old_clause = ast[(i-1)..(i+1)]
-        # Make sure we do any mapping
-        old_clause.each { |a| map(a) }
-        # Now create a new clause
-        new_clause = [:arithmetic, *old_clause]
-        # And insert it back into the ast
-        ast[(i-1)..(i+1)]  = [new_clause]
-        # Redo the mapping
-        map(ast)
-      end
+    return unless ast.size > 4
+    # This sets the operator precedence
+    i = nil
+    [{:'^' => 1},{:'*' => 2,:'/' => 2},{:'+' => 3,:'-' => 3}].each do |op|
+      i = ast.find_index { |a| a[0] == :operator && op.has_key?(a[1])}
+      break if i
     end
-    ast.each { |a| map(a) }
+    if i
+      # Now we need to wrap that operation in its own arithmetic clause
+      old_clause = ast[(i-1)..(i+1)]
+      # Make sure we do any mapping
+      #old_clause.each { |a| map(a) }
+      # Now create a new clause
+      new_clause = [:arithmetic, *old_clause]
+      # And insert it back into the ast
+      ast[(i-1)..(i+1)]  = [new_clause]
+      # Redo the mapping
+      simplify_arithmetic(ast)
+    end
   end
-    
 end
   
 
