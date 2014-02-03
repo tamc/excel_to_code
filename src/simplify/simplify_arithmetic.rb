fafd434db635798db38b8c85dfd5a0d61045e7d0
Tom Counsell <tom@counsell.org>
2014-02-03 09:27:28 +0000

    Reworked the SimplifyArithmetic routine to make fewer passes

--- src/simplify/simplify_arithmetic.rb
+++ src/simplify/simplify_arithmetic.rb
@@ -3,14 +3,12 @@ class SimplifyArithmeticAst
   attr_accessor :map_count  
 
   def map(ast)
-    @map_count = 0
     @brackets_to_remove = []
     simplify_arithmetic(ast)
     remove_brackets(ast)
   end
 
   def simplify_arithmetic(ast)
-    @map_count += 1
     return ast unless ast.is_a?(Array)
     ast.each { |a| simplify_arithmetic(a) }
     case ast[0]
@@ -27,26 +25,35 @@ class SimplifyArithmeticAst
     end
     ast
   end
+
+  # This sets the operator precedence
+  OPERATOR_PRECEDENCE = [
+    {:'^' => 1},
+    {:'*' => 2,:'/' => 2},
+    {:'+' => 3,:'-' => 3}
+  ]
   
   def arithmetic(ast)
+    # If smaller than 4, will only be a simple operation (e.g., 1+1 or 2*4)
+    # If more than 4, will be like 1+2*3 and so needs turning into 1+(2*3)
     return unless ast.size > 4
-    # This sets the operator precedence
-    i = nil
-    [{:'^' => 1},{:'*' => 2,:'/' => 2},{:'+' => 3,:'-' => 3}].each do |op|
-      i = ast.find_index { |a| a[0] == :operator && op.has_key?(a[1])}
-      break if i
+    OPERATOR_PRECEDENCE.each do |op|
+      i = nil
+      while i = ast.find_index { |a| a[0] == :operator && op.has_key?(a[1])}
+        # Now we need to wrap that operation in its own arithmetic clause
+        old_clause = ast[(i-1)..(i+1)]
+        # Make sure we do any mapping
+        #old_clause.each { |a| map(a) }
+        # Now create a new clause
+        new_clause = [:arithmetic, *old_clause]
+        # And insert it back into the ast
+        ast[(i-1)..(i+1)]  = [new_clause]
+        # Redo the mapping
+      end
     end
-    if i
-      # Now we need to wrap that operation in its own arithmetic clause
-      old_clause = ast[(i-1)..(i+1)]
-      # Make sure we do any mapping
-      #old_clause.each { |a| map(a) }
-      # Now create a new clause
-      new_clause = [:arithmetic, *old_clause]
-      # And insert it back into the ast
-      ast[(i-1)..(i+1)]  = [new_clause]
-      # Redo the mapping
-      simplify_arithmetic(ast)
+    # FIXME: this feels like a bodge
+    if ast.size == 2 && ast[0] == :arithmetic && ast[1].is_a?(Array) && ast[1][0] == :arithmetic
+      ast.replace(ast[1])
     end
   end
 end
