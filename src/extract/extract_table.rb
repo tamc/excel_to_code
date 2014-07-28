6a200a1a9c210dcb3eb01d4d36d352e3c10a90e1
Tom Counsell <tom@counsell.org>
2014-07-28 23:33:05 +0100

    Replaced Nokogiri with ox in ExtractTable

--- src/extract/extract_table.rb
+++ src/extract/extract_table.rb
@@ -1,12 +1,12 @@
-require 'nokogiri'
+require 'ox'
 
-class ExtractTable < Nokogiri::XML::SAX::Document 
+class ExtractTable < ::Ox::Sax
 
     def self.extract(*args)
       self.new.extract(*args)
     end
 
-    def extract(sheet_name, input)
+    def extract(sheet_name, input_xml)
       @sheet_name = sheet_name
       @output = {} 
       @table_name = nil
@@ -14,24 +14,55 @@ class ExtractTable < Nokogiri::XML::SAX::Document
       @table_total_rows = nil
       @table_columns = nil
 
-      @parsing = false
-      Nokogiri::XML::SAX::Parser.new(self).parse(input)
+      @state = :not_parsing
+      Ox.sax_parse(self, input_xml, :convert_special => true)
       @output
     end
     
-  def start_element(name,attributes)
-    if name == "table"
-      @table_name = attributes.assoc('displayName').last
-      @table_ref = attributes.assoc('ref').last
-      @table_total_rows = attributes.assoc('totalsRowCount').try(:last) || "0"
+  def start_element(name)
+    case name
+    when :table
+      @table_name = nil
+      @table_ref = nil
+      @table_total_rows = nil
       @table_columns = []
-    elsif name == "tableColumn"
-      @table_columns << attributes.assoc('name').last
+      @state = :parsing_table
+    when :tableColumn
+      @state = :parsing_table_column
+    else
+      @state = :not_parsing
+    end
+  end
+
+  def attr(name, value)
+    case @state
+    when :not_parsing
+      return
+    when :parsing_table_column
+      case name
+      when :name
+        @table_columns << value
+      end
+    when :parsing_table
+      case name
+      when :displayName
+        @table_name = value
+      when :ref
+        @table_ref = value
+      when :totalsRowCount
+        @table_total_rows = value
+      end
     end
   end
   
   def end_element(name)
-    return unless name == "table"
-    @output[@table_name] = [@sheet_name, @table_ref, @table_total_rows, *@table_columns]
+    case name
+    when :tableColumn
+      @state = :parsing_table
+    when :table
+      @output[@table_name] = [@sheet_name, @table_ref, @table_total_rows || "0", *@table_columns]
+      @state = :not_parsing
+    end
+
   end  
 end
