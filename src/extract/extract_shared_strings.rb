4836ccee9eb2871b8343cbb3ed8843b2dfecf03d
Tom Counsell <tom@counsell.org>
2014-07-28 23:07:34 +0100

    Replaced Nokogiri with Ox in ExtractSharedStrings

--- src/extract/extract_shared_strings.rb
+++ src/extract/extract_shared_strings.rb
@@ -1,30 +1,29 @@
-require 'nokogiri'
+require 'ox'
 
-class ExtractSharedStrings < Nokogiri::XML::SAX::Document 
+class ExtractSharedStrings < ::Ox::Sax
 
     def self.extract(input)
       self.new.extract(input)
     end
 
-    def extract(input)
-      @input, @output = input, []
+    def extract(input_xml)
+      @output = []
       @current = nil
-      parser = Nokogiri::XML::SAX::Parser.new(self)
-      parser.parse(input)
+      Ox.sax_parse(self, input_xml, :convert_special => true)
       @output
     end
 
-  def start_element(name,attributes)
-    @current = [] if name == "si"
+  def start_element(name)
+    @current = [] if name == :si
   end
   
   def end_element(name)
-    return unless name == "si"
+    return unless name == :si
     @output << [:string, @current.join]
     @current = nil
   end
   
-  def characters(string)
+  def text(string)
     return unless @current
     # FIXME: SHOULDN'T ELINMATE NEWLINES
     @current << string.gsub("\n","")
