eedffc811272158a6a32b203283d51d348e56a49
Tom Counsell <tom@counsell.org>
2014-08-14 20:48:50 +0100

    Removed redundant MapSheetNamesToCNames class

--- src/compile/c.rb
+++ src/compile/c.rb
@@ -1,6 +1,5 @@
 require_relative 'c/compile_to_c'
 require_relative 'c/compile_to_c_function'
 require_relative 'c/compile_to_c_header'
-require_relative 'c/map_sheet_names_to_c_names'
 require_relative "c/map_values_to_c_structs"
 require_relative "c/compile_named_reference_setters"
