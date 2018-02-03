bb15de6364fa40c005b8abc5a33845877291c8bc
Thomas Counsell <tom@eyfs.info>
2018-02-03 06:43:08 +0000

    Reformat and standardise command line help

--- src/commands/common_command_line_options.rb
+++ src/commands/common_command_line_options.rb
@@ -1,46 +1,76 @@
 class CommonCommandLineOptions
-  def self.set(command:, options:)
-    options.separator ''
-    options.separator 'Specific options:'
+  def self.set(command:, options:, generates:, extension:)
+    banner = <<-EOT
+      #{$0} [options] <excel_file> [output_directory]
 
-    options.on('-v', '--version', 'Prints the version number of this code') do
+      Roughly translate some Excel files into plain #{generates}
+
+      http://github.com/tamc/excel_to_code
+
+      Options:
+    EOT
+    options.banner = banner.split("\n").map(&:strip).join("\n")
+
+    options.on_tail('-v', '--version', '') do
       puts ExcelToCode.version
       exit
     end
 
-    options.on('-o', '--output-name NAME', 'Filename to give to c version of code (and associated ruby interface). Defaults to a folder with the same name as the excel file.') do |name|
+    options.on('-o', '--output-name NAME', "Name the generated code files. Default excelspreadsheet.#{extension}") do |name|
       command.output_name = name
     end
 
-    options.on('-r', '--run-tests', 'Compile the generated code and then run the tests') do
+    options.on('-r', '--run-tests', "Test whether the #{generates} matches the Excel.") do
       command.actually_run_tests = true
     end
 
-    options.on('-n', '--named-references', 'Transfer named references from spreadsheet to generated code') do
+    options.on('-n', '--named-references', "Include Excel named references as variables in the #{generates}.") do
       command.named_references_that_can_be_set_at_runtime = :where_possible
       command.named_references_to_keep = :all
     end
 
-    options.on('-s', '--settable WORKSHEET', 'Make it possible to set the values of cells in this worksheet at runtime. By default no values are settable.') do |sheet|
+    options.on('-s', '--settable INPUT_WORKSHEET', "Translate value cells in INPUT_WORKSHEET as settable variables in the #{generates}.") do |sheet|
+       
       command.cells_that_can_be_set_at_runtime = { sheet => :all }
     end
 
-    options.on('-p', '--prune-except WORKSHEET', 'Remove all cells except those on this worksheet, or that are required to calculate values on that worksheet. By default keeps all cells.') do |sheet|
+    options.on('-p', '--prune-except OUTPUT_WORKSHEET', 'Only translate OUTPUT_WORKSHEET and the cells its results depend on.') do |sheet|
       command.cells_to_keep = { sheet => :all }
     end
 
-    options.on('--isolate WORKSHEET', 'Only performs translation and optimiation of that one worksheet. Useful for debugging an incorrect translation of a large worksheet') do |sheet|
+    options.on('--isolate FAULTY_WORKSHEET', 'Only translate FAULTY_WORKSHEET. Useful for debugging.') do |sheet|
       command.isolate = sheet
     end
 
-    options.on('-d', '--debug', 'Does not perform final optimisations of spreadsheet, leaving the resulting code more similar to the original worksheet, but potentially slower') do |_sheet|
+    options.on('-d', '--debug', "Fewer optimisations; the #{generates} should be more similar to the original Excel.") do
       command.should_inline_formulae_that_are_only_used_once = false
       command.extract_repeated_parts_of_formulae = false
     end
 
-    options.on('-h', '--help', 'Show this message') do
+    options.on_tail('-h', '--help', '') do
       puts options
       exit
     end
+    
+    options.set_summary_width 35
+
+  end
+  
+  def self.parse(options:, command:, arguments:)
+    begin
+      options.parse!(arguments)
+    rescue OptionParser::ParseError => e 
+      STDERR.puts e.message, "\n", options 
+      return false
+    end
+
+    unless arguments.size > 0
+      puts options
+      return false
+    end
+
+    command.excel_file = arguments[0]
+    command.output_directory = arguments[1] if arguments[1]
+    return true
   end
 end
