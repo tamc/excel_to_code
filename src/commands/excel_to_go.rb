5bd47a294303542aae43b1a531472d4712cc4acd
Thomas Counsell <tom@eyfs.info>
2019-05-30 06:33:10 +0100

    Go: Linting

--- src/commands/excel_to_go.rb
+++ src/commands/excel_to_go.rb
@@ -1,26 +1,24 @@
-# coding: utf-8
+# frozen_string_literal: true
 
 require_relative 'excel_to_x'
 require 'pathname'
 
 class ExcelToGo < ExcelToX
-
   def language
     'go'
-  end  
-  
-  # Skip this
-  def replace_values_with_constants    
   end
-  
+
+  # Skip this
+  def replace_values_with_constants; end
+
   # These actually create the code version of the excel
   def write_code
     write_out_excel_as_code
     write_out_test_as_code
   end
-    
+
   def write_out_excel_as_code
-    log.info "Starting to write out code"
+    log.info 'Starting to write out code'
 
     f = "#{output_name.downcase}.go"
     o = output(f)
@@ -38,17 +36,17 @@ class ExcelToGo < ExcelToX
     o.puts
 
     o.puts excel_lib_functions
-    o.puts 
+    o.puts
 
     close(o)
-    log.info "Finished writing code"
-    
-    log.info "Running gofmt"
+    log.info 'Finished writing code'
+
+    log.info 'Running gofmt'
     log.info `gofmt -w -s #{f}`
   end
 
   def excel_lib
-    @excel_lib ||= IO.readlines(File.join(File.dirname(__FILE__),'..','compile','go','excel.go')).join
+    @excel_lib ||= IO.readlines(File.join(File.dirname(__FILE__), '..', 'compile', 'go', 'excel.go')).join
   end
 
   def excel_lib_imports
@@ -56,21 +54,21 @@ class ExcelToGo < ExcelToX
   end
 
   def excel_lib_functions
-    excel_lib[/import \(.*?\)(.*)/m,1]
+    excel_lib[/import \(.*?\)(.*)/m, 1]
   end
 
   def write_out_test_as_code
-    log.info "Starting to write out test"
-    
+    log.info 'Starting to write out test'
+
     f = "#{output_name.downcase}_test.go"
     o = output(f)
 
     o.puts "// Test of compiled version of #{excel_file}"
     o.puts "package #{output_name.downcase}"
     o.puts
-    o.puts "import ("
-    o.puts "    \"testing\""
-    o.puts ")"
+    o.puts 'import ('
+    o.puts '    "testing"'
+    o.puts ')'
     o.puts
 
     c = CompileToGoTest.new
@@ -80,19 +78,19 @@ class ExcelToGo < ExcelToX
     o.puts
 
     close(o)
-    log.info "Finished writing tests"
-    log.info "Running gofmt on tests"
+    log.info 'Finished writing tests'
+    log.info 'Running gofmt on tests'
     log.info `gofmt -w -s #{f}`
   end
-  
+
   def compile_code
     # Not needed
   end
-  
+
   def run_tests
     return unless actually_run_tests
-    log.info "Running the resulting tests"
+
+    log.info 'Running the resulting tests'
     log.info `cd #{File.join(output_directory)}; go test`
   end
 end
-
