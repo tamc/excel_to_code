e6ff383033093b53e8cec58f5fd0b70d8d02006e
Thomas Counsell <tom@eyfs.info>
2019-06-28 05:35:02 +0100

    Go: Lint map_values_to_go

--- src/compile/go/map_values_to_go.rb
+++ src/compile/go/map_values_to_go.rb
@@ -1,67 +1,63 @@
+# frozen_string_literal: true
+
 require_relative '../../util/not_supported_exception'
 
 class MapValuesToGo
-
   def map(ast)
-    if ast.is_a?(Array)
-      operator = ast[0]
-      if respond_to?(operator)
-        send(operator,*ast[1..-1])
-      else
-        raise NotSupportedException.new("#{operator} in #{ast.inspect} not supported")
-      end
-    else
-      raise NotSupportedException.new("#{ast} not supported")
-    end
+    raise NotSupportedException, "#{ast} not supported" unless ast.is_a?(Array)
+
+    operator = ast[0]
+    raise NotSupportedException, "#{operator} in #{ast} not supported" unless respond_to?(operator)
+
+    send(operator, *ast[1..-1])
   end
-  
+
   def blank
-    "Blank{}"
+    'Blank{}'
   end
 
   def inlined_blank
-    "Blank{}"
+    'Blank{}'
   end
-  
+
   def constant(name)
     name
   end
-  
-  alias :null :blank
-    
+
+  alias null blank
+
   def number(text)
     text.to_s
   end
-  
+
   def percentage(text)
     (text.to_f / 100.0).to_s
   end
-  
+
   def string(text)
     text.inspect
   end
-  
+
   ERRORS = {
-    :"#NAME?" => "NameError{}",
-    :"#VALUE!" => "ValueError{}",
-    :"#DIV/0!" => "Div0Error{}",
-    :"#REF!" => "RefError{}",
-    :"#N/A" => "NAError{}",
-    :"#NUM!" => "NumError{}"
-  }
-  
+    "#NAME?": 'NameError{}',
+    "#VALUE!": 'ValueError{}',
+    "#DIV/0!": 'Div0Error{}',
+    "#REF!": 'RefError{}',
+    "#N/A": 'NAError{}',
+    "#NUM!": 'NumError{}'
+  }.freeze
+
   REVERSE_ERRORS = ERRORS.invert
-  
+
   def error(text)
-    ERRORS[text.to_sym] || (raise NotSupportedException.new("#{text.inspect} error not recognised"))
+    ERRORS[text.to_sym] || (raise NotSupportedException, "#{text.inspect} error not recognised")
   end
-  
+
   def boolean_true
-    "true"
+    'true'
   end
-  
+
   def boolean_false
-    "false"
+    'false'
   end
-  
 end
